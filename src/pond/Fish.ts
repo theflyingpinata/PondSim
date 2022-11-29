9e432755cac30bce081e18205bf611bb34fd5aed
JeremiahClifford <63067769+JeremiahClifford@users.noreply.github.com>
2022-11-29 17:28:36 -0500

    Added my type stuff
    added the 2 files I made before so we can start working

---
+++ src/pond/Fish.ts
@@ -0,0 +1,26 @@
+//Plant population doesn't change, just exists to provide food for fish in a pond
+//Population is used to simulate how large of a fish population the pond can support
+type Plant = {
+    //Indentifier
+    Name: string
+    //Properties
+    Population: number
+}
+
+type Fish = {
+    //Identifier
+    Name: string
+    //Properties
+    Prey: Fish[] | Plant[]
+    FoodRequired: number
+    ReproductionLevel: number
+    ReproductionFrequency: number
+    DeathRate: number
+    //Data Trackers
+    Population: number
+    //Funtions
+    Feed: (rand: number) => void
+    Reproduce: (rand: number) => void
+}
+
+export type { Plant, Fish }
---
+++ src/pond/Pond.ts
@@ -0,0 +1,13 @@
+import { Plant, Fish } from "./Fish";
+
+type Pond = {
+    //Contents
+    Species: Fish[] | Plant[]
+    //Attributes
+    FishingRate: number
+    //Functions
+    RunTimeStep(): void
+    Feedings(): void
+    Reproductions(): void
+    Deaths(): void
+}
